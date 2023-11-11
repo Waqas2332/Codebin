0ed240bdf1a139b6342a094b9321e6678c71b648
Waqas <wmunir232@gmail.com>
2023-11-11 13:43:11 +0500

    Finished Styling

--- components/Icons.js
+++ components/Icons.js
@@ -1,6 +1,6 @@
 export default function Icon({ children, text }) {
   return (
-    <li className="mt-4 flex justify-center items-center flex-col text-gray-200 hover:text-white cursor-pointer">
+    <li className="mt-5 flex justify-center items-center flex-col text-gray-200 hover:text-white cursor-pointer">
       {children}
       <p>{text}</p>
     </li>
